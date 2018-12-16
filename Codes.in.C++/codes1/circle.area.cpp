b6c5fd9be9beb37b335e199cbca4e34257b125f3
Rafael <rgildo38@gmail.com>
2018-12-16 18:41:03 -0200

    Circle area

---
+++ Codes.in.C++/codes1/circle.area.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{ 
+    double r, pi = 3.14159;
+    scanf("%lf",&r);
+    printf("A=%.4lf\n",(pi*pow(r,2)));
+    return 0;
+}
+//codigo para calcular área de um circulo
