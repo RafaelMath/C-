08e67e447f29f64695ffb759655ddc97b50664b8
RafaelMath <rgildo38@gmail.com>
2018-12-25 19:09:22 -0300

    updating

---
+++ Codes.in.C++/codes1/Jogo.do.maior.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	int numRodada,a,b,i;
+	int rankA = 0,rankB = 0;
+	while(1){
+	
+	scanf("%d",&numRodada);
+		if(numRodada == 0)
+		return 0;
+		else
+	for(i = 1;i<=numRodada;i++){
+		scanf("%d""%d",&a,&b);
+			if(a > b)
+				rankA++;
+			else if(b > a)
+				rankB++;
+			else if(a == b)
+				continue;
+		
+	}
+	printf("%d %d\n",rankA,rankB);
+	rankA = 0;rankB = 0;
+	}
+return 0;}
