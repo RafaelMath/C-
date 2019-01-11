2b1fe18647c65bd2ded48989c5ca38092125628b
RafaelMath <rgildo38@gmail.com>
2019-01-10 22:16:47 -0300

    updating

--- Codes.in.C++/lessons/intervalo2.c
+++ Codes.in.C++/lessons/intervalo2.c
@@ -1,8 +1,19 @@
 #include<stdio.h>
 int main()
 {
-int N,teste;
-scanf("%d",&N);
-
+    int N,X,i;
+    int fora = 0;
+    int dentro = 0;
+    scanf("%d",&N);
+    for(i = 1; i<=N;++i)
+    {
+        scanf("%d",&X);
+        if(X>=10 && X<=20)
+            ++dentro;
+        else
+            ++fora;
+    }
+    printf("%d in\n",dentro);
+    printf("%d out\n",fora);
 return 0;
 }
