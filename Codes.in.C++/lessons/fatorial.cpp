ad51a42d24394ebc8ab7622e1440d06e823f63ac
RafaelMath <rgildo38@gmail.com>
2019-01-12 23:48:58 -0300

    lista

---
+++ Codes.in.C++/lessons/fatorial.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+	double  num,a,b;
+		scanf("%lf",&num);
+
+	if (num>0){
+		a=num;
+		b=num;
+		num--;
+
+	for (num;num>=1;num--)
+		a=a*num;
+		printf("%0.lf\n",a);}
+
+}
