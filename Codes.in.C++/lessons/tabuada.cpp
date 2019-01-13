5472a9721466cf368b0b108ded98f62b6e4e0cad
RafaelMath <rgildo38@gmail.com>
2019-01-12 22:31:41 -0300

    lesson

---
+++ Codes.in.C++/lessons/tabuada.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int i,numero;
+    scanf("%d",&numero);
+    for(i=1;i<=10;i++)
+        printf("%d x %d = %d\n",i,numero,numero*i);
+return 0;
+}
+//tabuada multiplicação
