7a64380c3ff0dce309479ef8871af9cde54e263e
RafaelMath <rgildo38@gmail.com>
2019-01-25 23:19:03 -0300

    Update qual_triangulo.c

--- Codes.in.C++/lessons/qual_triangulo.c
+++ Codes.in.C++/lessons/qual_triangulo.c
@@ -1,85 +1,63 @@
 #include<stdio.h>
-
 int main()
 {
-	float A, B, C;
+    //A, B, C are sides of any triangle
+    //algorithmo shows which triangle it's
 
+	float A, B, C;
 	scanf("%f %f %f", &A, &B, &C);
 
-		if(B<C)
-		 {
+	//sort: A is the largest and C is the smallest
+        if(B<C)
+        {
 			float temp = B;
-		  		B = C;
-				C = temp;
-		 }
+            B = C;
+            C = temp;
+        }
 		if(A<B)
 		{
 			float temp = A;
-				A = B;
-				B = temp;
+            A = B;
+            B = temp;
 		}
 
-		if(A>=B+C)
-		 {
+		if(A >= B + C)
 			printf("Invalido\n");
-			/*if((A==B)&&(B==C)&&(A==C))
-				printf("TRIANGULO EQUILATERO\n");
-			else if((A==B)||(B==C)||(A==C))
-				printf("TRIANGULO ISOSCELES\n");
-            */
-		 }
-
 		else if((A*A)==((B*B)+(C*C)))
-	 	 {
-			if((A==B)&&(B==C)&&(A==C)){
+        {
+			if((A==B)&&(B==C)&&(A==C))
+            {
 				printf("Valido-Equilatero\n");
-				printf("Retangulo: S\n");}
-			else if((A==B)||(B==C)||(A==C)){
+				printf("Retangulo: S\n");
+            }
+			else if((A==B)||(B==C)||(A==C))
+            {
 				printf("Valido-Isoceles\n");
-				printf("Retangulo: S\n");}
+				printf("Retangulo: S\n");
+            }
             else
             {
                 printf("Valido-Escaleno\n");
                 printf("Retangulo: S\n");
             }
-		 }
-		else{
-            if((A==B)&&(B==C)&&(A==C)){
+        }
+		else
+        {
+            if((A==B)&&(B==C)&&(A==C))
+            {
 				printf("Valido-Equilatero\n");
-				printf("Retangulo: N\n");}
-			else if((A==B)||(B==C)||(A==C)){
+				printf("Retangulo: N\n");
+            }
+			else if((A==B)||(B==C)||(A==C))
+            {
 				printf("Valido-Isoceles\n");
-				printf("Retangulo: N\n");}
+				printf("Retangulo: N\n");
+            }
             else
             {
                 printf("Valido-Escaleno\n");
                 printf("Retangulo: N\n");
             }
-		}
-	/*	else if((A*A)>((B*B)+(C*C)))
-		 {
-			printf("TRIANGULO OBTUSANGULO\n");
-			if((A==B)&&(B==C)&&(A==C))
-				printf("TRIANGULO EQUILATERO\n");
-			else if((A==B)||(B==C)||(A==C))
-				printf("TRIANGULO ISOSCELES\n");
-            else
-            {
-                printf("Valido-Escaleno\n");
-            }
-		 }
-
-		else if((A*A)<((B*B)+(C*C)))
-		 {
-			printf("TRIANGULO ACUTANGULO\n");
-			if((A==B)&&(B==C)&&(A==C))
-				printf("TRIANGULO EQUILATERO\n");
-			else if((A==B)||(B==C)||(A==C))
-				printf("TRIANGULO ISOSCELES\n");
-		 }
-    */
+        }
 return 0;
 }
-
-//"Valido-Equilatero", "Valido-Escaleno" ou "Valido-Isoceles"
-// "Retangulo: S" se o triângulo for retângulo ou "Retangulo: N"
