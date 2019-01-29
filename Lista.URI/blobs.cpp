f548f3e90af94621536c281167af059da0051758
Rafael <36546806+RafaelMath@users.noreply.github.com>
2019-01-28 22:27:56 -0200

    Update blobs.cpp

--- Lista.URI/blobs.cpp
+++ Lista.URI/blobs.cpp
@@ -1,11 +1,12 @@
-//código dos Blobs
+//cÃ³digo dos Blobs
 //Blobs comem 1/2 da comida por dia
 #include <stdio.h>
 int main(){
 int contador, i = 1;
     scanf("%d",&contador);
     for(i; i <= contador; i++)
-    {   int dias = 0;
+    {   
+        int dias = 0;
         float comidaKg;
         scanf("%f",&comidaKg);
         for(dias;comidaKg>1;dias++)
